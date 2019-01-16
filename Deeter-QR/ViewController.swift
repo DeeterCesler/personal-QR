5858621cce79e9de80f93fb0ebfed83aef8dad6f
Deeter <deeter.cesler@gmail.com>
2019-01-16 11:44:20 -0700

    my personal QR app

---
+++ Deeter-QR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deeter-QR
+//
+//  Created by Deeter Cesler on 1/15/19.
+//  Copyright © 2019 Deeter Cesler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
